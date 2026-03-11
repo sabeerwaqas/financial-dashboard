68849dff00309eb23b3e48930c67feb43365f3d9
Sabeerwaqas <sabeerwaqas050@gmail.com>
2026-03-11 22:29:03 +0500

    refactor: create table directory and move all tables inside it

--- frontend/component/invoices/index.ts
+++ frontend/component/invoices/index.ts
@@ -1,4 +1,4 @@
-export * from './table';
-export * from './pagination';
+export * from "../table/invoice-table";
+export * from "./pagination";
 export * from "./edit-form";
 export * from "./create-form";
