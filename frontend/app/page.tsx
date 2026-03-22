aae012938ca733e10cf2add78ea4f675a1e761ae
Sabeerwaqas <sabeerwaqas050@gmail.com>
2026-03-22 21:54:01 +0500

    chore: show redirect to dashboard button on home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,8 @@
-import AcmeLogo from '@/component/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import { lusitana } from '@/component/fonts';
-import Image from 'next/image';
+import AcmeLogo from "@/component/acme-logo";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+import { lusitana } from "@/component/fonts";
+import Image from "next/image";
 
 export default function Page() {
   return (
@@ -15,17 +15,18 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Welcome to Acme.</strong> This is the example for the{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
             , brought to you by Vercel.
           </p>
           <Link
-            href="/login"
+            href="/dashboard"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>Visit Dashboard</span>{" "}
+            <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
